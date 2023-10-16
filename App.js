eb8cb4030ccbb71f3c30a5bfa29c7aacc224dc78
garry <44156478+garygaurav13@users.noreply.github.com>
2023-10-16 14:49:02 +0530

    ReactNative Developer (16thOct2023)

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from "react";
+import { View, SafeAreaView, Image, TextInput, Button, Switch } from 'react-native';
+import Home from "./screens/home";
+import AvtarImage from "./assets/logo3.png";
 
 export default function App() {
+  const [active, setActive] = useState(false);
+  console.log(active);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <SafeAreaView>
+        <Home />
+        <Image 
+          source={ AvtarImage }
+          style={{ width:301, height:301 }} 
+        />
+        <TextInput
+          style={{ backgroundColor: "lightgray", padding:20, color: "white", borderColor:"black" }}
+          placeholder="Input Filed"
+        />
+        <Button title='Click Me' onPress={() => console.log("Btn Pressed")}></Button>
+        <Switch value={active} onChange={()=>setActive((prev) => !prev)} />
+      </SafeAreaView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
